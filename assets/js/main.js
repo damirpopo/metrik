9d08fea28c3979ec18c9de4db481157167211107
damirpopo <43522476+damirpopo@users.noreply.github.com>
2022-11-11 22:43:14 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+// document.body.classList.add('www');
+// alert(document.body.className);
+
+
+// document.body.classList.add('www');
+// document.body.classList.remove('www');
+// alert(document.body.className);
+
+
+// alert(document.body.classList.contains("www"))
+
+
+// document.body.classList.toggle('www')
+
+// alert(document.body.className)
+
+// for (let name of document.body.classList) {
+//     alert(name);
+
+
+// elem2.style.cssText=`color: red;
+// width: 40px;
+// `;
+// alert(elem2.style.cssText);
+
+// function elemclick(){
+//     alert(document.body.tagName)
+// }
+
+// function elemclick(){
+//     console.log(document.body.tagName)
+// }
+ 
+// let ol=document.getElementById("elem4");
+// let li=document.createElement('li')
+// li.innerHTML='пункт'
+// ol.appendChild(li);
+
+// let arr=['sada','dadw','dawd']
+// for (let i=0; i<arr.length;i++){
+//     let li=document.createElement('li')
+//     let ul=document.getElementById('elem5')
+//     li.innerHTML=arr[i];
+//     ul.appendChild(li)
+// }
+
+// let elem2=document.getElementById('elem2')
+// elem2.insertAdjacentHTML("beforebegin","<span>'!!!'</span>");
+
+// let elem2=document.getElementById('elem2')
+// elem2.insertAdjacentHTML("afterbegin","<span>'!!!'</span>");
+
+// let elem2=document.getElementById('elem2')
+// elem2.insertAdjacentHTML("beforeend","<span>'!!!'</span>");
+
+// let elem2=document.getElementById('elem2')
+// elem2.insertAdjacentHTML("afterend","<span>'!!!'</span>");
+
+// let parent=document.getElementById('parent')
+// let child=document.getElementById('child')
+// function elemclick(){
+//     parent.removeChild(child)
+// }
+
+// let ol_elem=document.getElementById('elem4')
+// function elemclick(){
+//     ol_elem.removeChild(ol_elem.lastElementChild)
+// }
+
+// let clone=document.getElementById('elem9')
+// function elemclick(){
+//     let cclone= clone.cloneNode(true);
+//     clone.parentElement.appendChild(cclone)
+// }
+
+// let red=document.querySelector("elem10")
+// red.firstChild.style.color =`red`;
+
+// const red1=document.querySelector("elem10")
+// red1.lastElementChild.style.color =`red`;
+
+// let wq=document.getElementById('elem5')
+// wq.classList.add('!!')
+
